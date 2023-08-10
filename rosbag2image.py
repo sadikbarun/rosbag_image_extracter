a1915489b78927bfcc63a8733fb788bb871f235d
Sadık Barun <75198383+sadikbarun@users.noreply.github.com>
2023-08-10 14:29:34 +0300

    Update rosbag2image.py
     comments added

--- rosbag2image.py
+++ rosbag2image.py
@@ -6,20 +6,20 @@ from cv_bridge import CvBridge
 import numpy as np
 
 
-FILENAME = 'test'
-ROOT_DIR = '/home/sadik/noetic_ws'
+FILENAME = 'test'                                        # rosbag file name without .bag
+ROOT_DIR = '/home/sadik/noetic_ws'                        # rosbag path
 BAGFILE = ROOT_DIR + '/' + FILENAME + '.bag'
-f = open(ROOT_DIR + '/images/tst/' + FILENAME + '-' + 'timestamps.txt', 'w')
+f = open(ROOT_DIR + FILENAME + '-' + 'timestamps.txt', 'w')    # for saving timestamps
 if __name__ == '__main__':
     bag = rosbag.Bag(BAGFILE)
-    TOPIC = '/usb_cam/image_raw'
+    TOPIC = '/usb_cam/image_raw'                        # image topic
     image_topic = bag.read_messages(TOPIC)
     for k, b in enumerate(image_topic):
         bridge = CvBridge()
         cv_image = bridge.imgmsg_to_cv2(b.message, b.message.encoding)
         cv_image.astype(np.uint8)
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(ROOT_DIR + '/images/tst/' + str(b.timestamp) + '.png', cv_image)
+        cv2.imwrite(ROOT_DIR + str(b.timestamp) + '.png', cv_image)
         print('saved: ' + str(b.timestamp) + '.png')
         f.write(str(b.timestamp) + '\n')
 
