49aa1f2cfa5ca68844c8f86b0ea98c8fff44ca51
Sadık Barun <75198383+sadikbarun@users.noreply.github.com>
2023-08-10 14:23:16 +0300

    Add files via upload

---
+++ rosbag2image.py
@@ -0,0 +1,28 @@
+import subprocess
+import yaml
+import rosbag   # source /opt/ros/noetic/setup.bash
+import cv2
+from cv_bridge import CvBridge
+import numpy as np
+
+
+FILENAME = 'test'
+ROOT_DIR = '/home/sadik/noetic_ws'
+BAGFILE = ROOT_DIR + '/' + FILENAME + '.bag'
+f = open(ROOT_DIR + '/images/tst/' + FILENAME + '-' + 'timestamps.txt', 'w')
+if __name__ == '__main__':
+    bag = rosbag.Bag(BAGFILE)
+    TOPIC = '/usb_cam/image_raw'
+    image_topic = bag.read_messages(TOPIC)
+    for k, b in enumerate(image_topic):
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(b.message, b.message.encoding)
+        cv_image.astype(np.uint8)
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(ROOT_DIR + '/images/tst/' + str(b.timestamp) + '.png', cv_image)
+        print('saved: ' + str(b.timestamp) + '.png')
+        f.write(str(b.timestamp) + '\n')
+
+    bag.close()
+
+    print('PROCESS COMPLETE')
